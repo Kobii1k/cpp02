e14d014b1f6ff13d3e3ff932f8e617783a0ea291
Maxens Gagne <mgagne@z3r9p5.42lyon.fr>
2024-01-18 10:55:15 +0100

    main 02

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mgagne <mgagne@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/10 15:53:59 by mgagne            #+#    #+#             */
-/*   Updated: 2024/01/11 18:56:53 by mgagne           ###   ########.fr       */
+/*   Updated: 2024/01/18 10:49:35 by mgagne           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,10 @@ int main()
 {
 	Fixed a;
 	Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
+	Fixed c(10.5f);
+	Fixed d(15);
+	Fixed e(10.5f);
+
 	std::cout << a << std::endl;
 	std::cout << ++a << std::endl;
 	std::cout << a << std::endl;
@@ -23,5 +27,25 @@ int main()
 	std::cout << a << std::endl;
 	std::cout << b << std::endl;
 	std::cout << Fixed::max( a, b ) << std::endl;
+
+	std::cout << "(10.5 < 15)	:" << (c < d) << std::endl;
+	std::cout << "(15 < 10.5)	:" << (d < c) << std::endl;
+	std::cout << "(15 > 10.5)	:" << (d > c) << std::endl;
+	std::cout << "(10.5 > 15)	:" << (c > d) << std::endl;
+
+	std::cout << "(10.5 >= 10.5)	:" << (c >= e) << std::endl;
+	std::cout << "(10.5 >= 10.5)	:" << (e >= c) << std::endl;
+	std::cout << "(10.5 <= 10.5)	:" << (c <= e) << std::endl;
+	std::cout << "(10.5 <= 10.5)	:" << (e <= c) << std::endl;
+
+	std::cout << "(10.5 == 10.5)	:" << (c == e) << std::endl;
+	std::cout << "(15 == 10.5)	:" << (d == c) << std::endl;
+
+	std::cout << "min(0.0078125, 10.1016)	:" << Fixed::min(a, b) << std::endl;
+
+	std::cout << "a	=" << a << std::endl;
+	std::cout << "--a	=" << --a << std::endl;
+	std::cout << "a--	=" << a-- << std::endl;
+	std::cout << "a	=" << a << std::endl;
 	return 0;
 }
