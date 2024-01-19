ed37ff5c5bd7e3b471734ea71c376ebe0d3f413c
Maxens Gagne <mgagne@z4r6p4.42lyon.fr>
2024-01-19 10:26:43 +0100

    prints

--- ex02/Fixed.cpp
+++ ex02/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: mgagne <mgagne@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/10 15:54:24 by mgagne            #+#    #+#             */
-/*   Updated: 2024/01/18 10:31:13 by mgagne           ###   ########.fr       */
+/*   Updated: 2024/01/19 10:26:00 by mgagne           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,48 +14,40 @@
 
 Fixed::Fixed(void)
 {
-	std::cout << "Default constructor called" << std::endl;
 	nb = 0;
 }
 
 Fixed::~Fixed(void)
 {
-	std::cout << "Destructor called" << std::endl;
 }
 
 Fixed::Fixed(const Fixed &new_cpy)
 {
-	std::cout << "Copy constructor called" << std::endl;
 	nb = new_cpy.nb;
 }
 
 Fixed 	&Fixed::operator=(const Fixed &new_cpy)
 {
-	std::cout << "Copy assignment operator called" << std::endl;
 	this->setRawBits(new_cpy.getRawBits());
 	return (*this);
 }
 
 Fixed::Fixed(const int new_nb)
 {
-	std::cout << "Int constructor called" << std::endl;
 	nb = (new_nb * (1 << fixedBits));
 }
 Fixed::Fixed(const float new_nb)
 {
-	std::cout << "Float constructor called" << std::endl;
 	nb = roundf(new_nb * (1 << fixedBits));
 }
 
 void	Fixed::setRawBits(int const raw)
 {
-	std::cout << "setRawBits member function called" << std::endl;
 	nb = raw;
 }
 
 int		Fixed::getRawBits(void) const
 {
-	std::cout << "getRawBits member function called" << std::endl;
 	return (nb);
 }
 
